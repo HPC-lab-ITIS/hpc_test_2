3bf4b4e82fd1ca6fea568979db7d23291e0344f1
Eduard Kh <ekhramch@gmail.com>
2017-02-21 09:58:34 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#define __CL_ENABLE_EXCEPTIONS
+#include "CL/cl.hpp"
+#include <iostream>
+#include <vector>
+#include "profiler.h"
+#include <algorithm>
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
